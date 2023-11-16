4924e675e7bcff7128c81d04d96c470d2dc24294
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2023-11-16 23:09:14 +0100

    screw that, rethink it

--- index.js
+++ index.js
@@ -25,34 +25,18 @@ function visualizer(stream) {
     const analyser = audioCtx.createAnalyser();
     const source = audioCtx.createMediaStreamSource(stream);
     source.connect(analyser);
-    analyser.fftSize = 32;
+    analyser.fftSize = 128;
     animate(analyser);
 }
-
-let prev = [];
 
 function animate(analyser) {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
     analyser.getByteFrequencyData(dataArray);
-    let freq_range = 0;
-    let freq = 0;
+
     for (let i = 0; i < bufferLength; i++) { 
-        if (i % (bufferLength / 8) == 0 && i != 0) {
-            let value = (freq / bufferLength / 8);
-            if (value < 2 || Math.abs(prev[freq_range] - value) < 8) {
-                continue;
-            }
-            prev[freq_range] = value;
-            value = (value - 2) * 10
-            let doc = document.getElementById('char'+freq_range);
-            doc.innerText = chars[(Math.round(value) % chars.length)];
-            doc.classList.value = 'text-violet-' + ((freq % 4) * 100 + 500)
-            freq_range++;
-        }
-        freq += dataArray[i];
+
     }
-    prev = dataArray;
     requestAnimationFrame(() => animate(analyser));
 }
 
